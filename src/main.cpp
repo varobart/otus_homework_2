2839d400e5c6184eca9ee8337f4f950e59b1ba1d
varobart <varabart@gmail.com>
2021-10-20 18:44:26 +0700

    Abstracted logging logic

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,51 @@
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
+
+class ILogger {
+public:
+  ILogger() = default;
+  virtual ~ILogger() = default;
+  virtual void log(const std::string &message) = 0;
+};
+
+class ConsoleLogger : public ILogger {
+public:
+  ConsoleLogger() = default;
+  ~ConsoleLogger() = default;
+
+  void log(const std::string &message) override;
+};
+
+void ConsoleLogger::log(const std::string &message) {
+  std::cout << message << std::endl;
+}
+
+class FileLogger : public ILogger {
+public:
+  FileLogger();
+  ~FileLogger() = default;
+
+  void log(const std::string &message) override;
+
+private:
+  std::time_t m_time;
+};
+
+FileLogger::FileLogger() : m_time(std::time(nullptr)) {}
+
+void FileLogger::log(const std::string &message) {
+  std::string timeStr = std::to_string(static_cast<long int>(m_time));
+  std::string filename = "bulk" + timeStr + ".log";
+  std::ofstream file(filename);
+  file << message << std::endl;
+
+  m_time = std::time(nullptr);
+}
 
 class Bulk {
 public:
@@ -20,30 +62,27 @@ private:
   void log();
 
   std::stringstream m_cmds;
-
   size_t m_buffer_size = 0;
   size_t m_nCmds = 0;
-  std::time_t m_time;
   int m_nestingLvl = 0;
+  std::vector<std::unique_ptr<ILogger>> m_apLoggers;
 };
 
-Bulk::Bulk(size_t buffer_size)
-    : m_time(std::time(nullptr)), m_buffer_size(buffer_size) {
+Bulk::Bulk(size_t buffer_size) : m_buffer_size(buffer_size) {
   m_cmds << "bulk:";
+  m_apLoggers.emplace_back(std::make_unique<ConsoleLogger>());
+  m_apLoggers.emplace_back(std::make_unique<FileLogger>());
 }
 
 void Bulk::log() {
   if (m_nCmds > 0) {
-    std::string timeStr = std::to_string(static_cast<long int>(m_time));
-    std::string filename = "bulk" + timeStr + ".log";
-    std::ofstream file(filename);
-    file << m_cmds.str() << std::endl;
-    std::cout << m_cmds.str() << std::endl;
+    std::string message = m_cmds.str();
+    for (auto &pLogger : m_apLoggers)
+      pLogger->log(message);
   }
 
   m_nestingLvl = 0;
   m_nCmds = 0;
-  m_time = std::time(nullptr);
   m_cmds = std::stringstream();
   m_cmds << "bulk:";
 }
@@ -66,8 +105,6 @@ void Bulk::process(const std::string &cmd) {
   } else {
     if (m_nCmds > 0)
       m_cmds << ",";
-    else
-      m_time = std::time(nullptr);
 
     m_cmds << " " << cmd;
     ++m_nCmds;
